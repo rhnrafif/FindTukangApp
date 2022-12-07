939b45b2417e027b9a5339a1794feb4115836014
raihanudin rafif <raihanudinrafif@gmail.com>
2022-12-07 13:52:55 +0700

    [FEATURE] add Customer Controller

---
+++ FindTheBuilder/Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using FindTheBuilder.Applications.Services.CustomerAppServices;
+using FindTheBuilder.Applications.Services.CustomerAppServices.DTO;
+using FindTheBuilder.Applications.Services.TransactionAppServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FindTheBuilder.Controllers
+{
+	[Route("api/customer")]
+	[ApiController]
+	public class CustomerController : ControllerBase
+	{
+		private readonly ICustomerAppService _customerAppService;
+		private readonly ITransactionAppService _transactionAppService;
+		public CustomerController(ICustomerAppService customerAppService, ITransactionAppService transactionAppService)
+		{
+			_customerAppService = customerAppService;
+			_transactionAppService = transactionAppService;
+		}
+
+		[HttpPost("create-customer")]
+		public IActionResult CreateCustomer([FromBody] CustomerDTO model)
+		{
+			try
+			{
+				_customerAppService.Create(model);
+				return Ok(new { Message = "Succes" });
+			}
+			catch
+			{
+				return Ok(new { Message = "Succes" });
+			}
+		}
+	}
+}

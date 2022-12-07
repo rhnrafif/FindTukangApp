ab2717f6575b4f0e5e33a2df8cd7ed8a15988a2b
raihanudin rafif <raihanudinrafif@gmail.com>
2022-12-07 15:29:13 +0700

    [IMPROVE] add Auth on Controller

--- FindTheBuilder/Controllers/CustomerController.cs
+++ FindTheBuilder/Controllers/CustomerController.cs
@@ -8,6 +8,7 @@ using FindTheBuilder.Applications.Services.TransactionAppServices.DTO;
 using FindTheBuilder.Applications.Services.TransactionDetailAppServices;
 using FindTheBuilder.Applications.Services.TransactionDetailAppServices.DTO;
 using FindTheBuilder.Databases.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FindTheBuilder.Controllers
@@ -34,12 +35,14 @@ namespace FindTheBuilder.Controllers
 
 		// Customer
 		[HttpPost("CreateCustomer")]
+		[Authorize(Roles = "Customer")]
 		public Customers CreateCustomer([FromBody] CustomerDTO model)
 		{
 			return _customerAppService.Create(model);
 		}
 		
 		[HttpPatch("UpdateCustomer")]
+		[Authorize(Roles = "Customer")]
 		public Customers UpdateCustomer([FromBody] UpdateCustomerDTO model)
 		{
 			return _customerAppService.Update(model);
@@ -47,12 +50,14 @@ namespace FindTheBuilder.Controllers
 
 		// Transaction
 		[HttpPost("CreateTransaction")]
+		[Authorize(Roles = "Customer")]
 		public Transactions CreateTransaction([FromBody] TransactionDTO model)
 		{
 			return _transactionAppService.Create(model);
 		}
 
 		[HttpPatch("UpdateTransaction")]
+		[Authorize(Roles = "Customer")]
 		public Transactions UpdateTransaction([FromBody] UpdateTransactionDTO model)
 		{
 			return _transactionAppService.Update(model);
@@ -60,6 +65,7 @@ namespace FindTheBuilder.Controllers
 
 		// Transaction Detail
 		[HttpGet("GetAllTransaction")]
+		[Authorize(Roles = "Customer")]
 		public PagedResult<TransactionDetailDTO> GetAllTransaction([FromQuery] PageInfo pageInfo)
 		{
 			return _transactionDetailAppService.GetAllTransactions(pageInfo);
@@ -67,6 +73,7 @@ namespace FindTheBuilder.Controllers
 
 		// Prices
 		[HttpGet("GetAllPrice")]
+		[AllowAnonymous]
 		public PagedResult<PriceListDTO> GetAllPrice([FromQuery] PageInfo pageInfo)
 		{
 			return _priceAppService.GetPriceByProduct(pageInfo);
